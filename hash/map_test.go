667d86a924e932e7b7fa5e6b4af4f30ffa67238a
wdsgyj <wdsgyj@126.com>
2014-11-05 00:07:34 +0800

    添加浮点数 hash 算法 添加 Map 单元测试代码

--- hash/map_test.go
+++ hash/map_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestMap(t *testing.T) {
+func TestIntMap(t *testing.T) {
 	m1 := make(map[int]string, 100)
 	m2 := NewSizedHashMap(100)
 
@@ -46,6 +46,59 @@ func TestMap(t *testing.T) {
 
 	for i := 0; i < 100; i++ {
 		if m2.Remove(Int(i)).(string) != fmt.Sprintf("%d", i*10) {
+			t.Fatal("Map Remove 函数失败")
+		}
+
+		if m2.Size() != 100-i-1 {
+			t.Fatal("Map Remove 函数失败")
+		}
+	}
+
+	if !m2.IsEmpty() {
+		t.Fatal("Map IsEmpty 函数失败")
+	}
+}
+
+func TestStringMap(t *testing.T) {
+	m1 := make(map[string]string, 100)
+	m2 := NewSizedHashMap(100)
+
+	for i := 0; i < 100; i++ {
+		m2.Put(String(fmt.Sprintf("%d", i)), fmt.Sprintf("%d", i*10))
+
+		if m2.Size() != i+1 {
+			t.Fatal("Map Size 函数测试失败")
+		}
+	}
+
+	m2.Clear()
+	if !m2.IsEmpty() {
+		t.Fatal("Map IsEmpty Clear  函数失败")
+	}
+
+	for i := 0; i < 100; i++ {
+		m1[fmt.Sprintf("%d", i)] = fmt.Sprintf("%d", i*10)
+		m2.Put(String(fmt.Sprintf("%d", i)), fmt.Sprintf("%d", i*10))
+
+		if m2.Size() != i+1 {
+			t.Fatal("Map Size 函数测试失败")
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		if m1[fmt.Sprintf("%d", i)] != m2.Get(String(fmt.Sprintf("%d", i))).(string) {
+			t.Fatal("Map Get 函数失败")
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		if !m2.Contains(String(fmt.Sprintf("%d", i))) {
+			t.Fatal("Map Contains 函数失败")
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		if m2.Remove(String(fmt.Sprintf("%d", i))).(string) != fmt.Sprintf("%d", i*10) {
 			t.Fatal("Map Remove 函数失败")
 		}
 
