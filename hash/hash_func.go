9904b30c7851713509e5c7063cdd2d95b6558a18
clark <wdsgyj@126.com>
2014-11-11 16:16:33 +0800

    修改浮点类型和复数类型的 hash 方法

--- hash/hash_func.go
+++ hash/hash_func.go
@@ -1,9 +1,7 @@
 // hash_func
 package hash
 
-import (
-	"fmt"
-)
+import "unsafe"
 
 func HashInt(code uint32, n int) uint32 {
 	return 53*code + uint32(n)
@@ -58,27 +56,31 @@ func HashUInt64(code uint32, n uint64) uint32 {
 }
 
 func HashFloat32(code uint32, f float32) uint32 {
-	s := fmt.Sprintf("%f", f)
-	return HashString(code, s)
+	p := (*int32)(unsafe.Pointer(&f))
+	return HashInt32(code, *p)
 }
 
 func HashFloat64(code uint32, f float64) uint32 {
-	s := fmt.Sprintf("%f", f)
-	return HashString(code, s)
+	p := (*int64)(unsafe.Pointer(&f))
+	return HashInt64(code, *p)
 }
 
 func HashComplex64(code uint32, cpx complex64) uint32 {
-	s := fmt.Sprintf("%f", real(cpx))
-	code = HashString(code, s)
-	s = fmt.Sprintf("%f", imag(cpx))
-	return HashString(code, s)
+	r := real(cpx)
+	i := imag(cpx)
+	p := (*int32)(unsafe.Pointer(&r))
+	code = HashInt32(code, *p)
+	p = (*int32)(unsafe.Pointer(&i))
+	return HashInt32(code, *p)
 }
 
 func HashComplex128(code uint32, cpx complex128) uint32 {
-	s := fmt.Sprintf("%f", real(cpx))
-	code = HashString(code, s)
-	s = fmt.Sprintf("%f", imag(cpx))
-	return HashString(code, s)
+	r := real(cpx)
+	i := imag(cpx)
+	p := (*int64)(unsafe.Pointer(&r))
+	code = HashInt64(code, *p)
+	p = (*int64)(unsafe.Pointer(&i))
+	return HashInt64(code, *p)
 }
 
 func HashString(code uint32, s string) uint32 {
